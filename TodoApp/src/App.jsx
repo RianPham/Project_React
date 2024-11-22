a7872b3b28887b64b36ea01b577acb2bf2df5f0c
RianPham <huynhnhu20050102@gmail.com>
2024-11-22 22:23:28 +0700

    keycode

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -1,11 +1,17 @@
 import { PiAirplaneTiltFill } from 'react-icons/pi'
+import * as KeyCode from 'keycode-js';
 
 function App() {
+  console.log(KeyCode.KEY_A); // 65
+  console.log(KeyCode.KEY_END); // 35
+
   return (
     <>
-      <h1>hello world</h1>
+      <h1>hello</h1>
       <h2>Xin chao toi la Rian Pham</h2>
       <PiAirplaneTiltFill />
+
+
     </>
   )
 }
