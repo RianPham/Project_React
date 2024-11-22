69ba4d382966739397b0eba0f9a824ddd103e13c
RianPham <huynhnhu20050102@gmail.com>
2024-11-22 21:18:03 +0700

    install react icons

--- TodoApp/src/App.jsx
+++ TodoApp/src/App.jsx
@@ -1,10 +1,11 @@
-
+import { PiAirplaneTiltFill } from 'react-icons/pi'
 
 function App() {
   return (
     <>
       <h1>hello world</h1>
       <h2>Xin chao toi la Rian Pham</h2>
+      <PiAirplaneTiltFill />
     </>
   )
 }
